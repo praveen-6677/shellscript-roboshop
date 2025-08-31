bf7e9343a03f202231afada1deba7d4d8a8fa4d7
sairam <r.sairam514@gmail.com>
2025-08-31 19:42:31 +0530

    adding complete file

--- web.sh
+++ web.sh
@@ -18,6 +18,28 @@ if  [ $ID -ne 0 ]
   fi   
 
  dnf install nginx -y  
- validate $? "installing nginix"
+ VALIDATE $? "installing nginix"
  
+ systemctl enable nginx
+ VALIDATE $? "enabling nginx"
+
+ systemctl start nginx
+ VALIDATE $? "start nginx"
+
+ rm -rf /usr/share/nginx/html/*
+ VALIDATE $? "removing files"
+
+ unzip /tmp/web.zip
+ VALIDATE $? "unziping files"
+
+ cp /home/ec-2user/roboshop.conf /etc/nginx/default.d
+ VALIDATE $? "copying config file"
+
+
+
+systemctl restart nginx 
+
+ VALIDATE $? "restarting nginix"
+
+
 
