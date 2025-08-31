c388b18834c1fbc6a46662c0705a9c4e174544c1
sairam <r.sairam514@gmail.com>
2025-08-31 17:03:02 +0530

    adding file

--- web.sh
+++ web.sh
@@ -7,13 +7,15 @@ if [ $1 -ne 0 ]
    echo :  "installing $2 failed"
   else
   echo : "installing $2 success" 
-}
+ fi 
+ }
 if  [ $ID -ne 0 ]    
   then
      echo "run the script with root user"
    exit 1  
    else 
      echo " you are root user "
+  fi   
 
  dnf install nginx -y  
  validate $? "installing nginix"
